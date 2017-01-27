9c5a32f31eb3ab299fffc3c820d117fc98cb8872
Ira <smalko.irina.s@gmail.com>
2017-01-27 16:12:38 +0200

    all done

--- app/src/main/java/com/exemple/android/cookbook/AddCategoryRecipeActivity.java
+++ app/src/main/java/com/exemple/android/cookbook/AddCategoryRecipeActivity.java
@@ -1,6 +1,7 @@
 package com.exemple.android.cookbook;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,63 +12,41 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.exemple.android.cookbook.supporting.CategoryRecipes;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.util.Random;
 
 public class AddCategoryRecipeActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    static final int GALLERY_REQUEST = 1;
-    private final int CAMERA_RESULT = 0;
+    private static final int GALLERY_REQUEST = 1;
+    private static final int CAMERA_RESULT = 0;
+    private static final int PIC_CROP = 2;
 
     private TextView txtDetails;
     private EditText inputCategoryName;
-    private Button btnSave;
-    private ImageButton btnPhoto_fromGallery, btnPhoto_fromCamera;
-    private String recipeId;
-    private Bitmap bitmap = null;
-    private ImageView imageView;
 
     private DatabaseReference databaseReference;
-    private FirebaseDatabase firebaseDatabase;
-    private FirebaseStorage firebaseStorage;
     private StorageReference storageReference;
-    private Uri downloadUrl;
-    private CategoryRecipes categoryRecipes;
+    private Uri downloadUrlCamera;
     private String pictureImagePath = Environment.getExternalStoragePublicDirectory(
             Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/n" + ".jpg";
-
-    public void openBackCamera() {
-        File file = new File(pictureImagePath);
-        Uri outputFileUri = Uri.fromFile(file);
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-        startActivityForResult(cameraIntent, CAMERA_RESULT);
-    }
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -76,9 +55,9 @@ public class AddCategoryRecipeActivity extends AppCompatActivity {
 
         txtDetails = (TextView) findViewById(R.id.txt_category_recipe);
         inputCategoryName = (EditText) findViewById(R.id.name);
-        btnPhoto_fromGallery = (ImageButton) findViewById(R.id.categoryRecipesPhotoUrlGallery);
-        btnPhoto_fromCamera = (ImageButton) findViewById(R.id.categoryRecipesPhotoUrl);
-        btnSave = (Button) findViewById(R.id.btn_save);
+        ImageButton btnPhoto_fromGallery = (ImageButton) findViewById(R.id.categoryRecipesPhotoUrlGallery);
+        ImageButton btnPhoto_fromCamera = (ImageButton) findViewById(R.id.categoryRecipesPhotoUrl);
+        Button btnSave = (Button) findViewById(R.id.btn_save);
 
         btnPhoto_fromGallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,141 +72,107 @@ public class AddCategoryRecipeActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                openBackCamera();
+                photoFromCamera();
             }
         });
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseStorage = FirebaseStorage.getInstance();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
 
         databaseReference = firebaseDatabase.getReference("Сategory_Recipes");
         storageReference = firebaseStorage.getReference().child("Photo_Сategory_Recipes");
         firebaseDatabase.getReference("app_title").setValue("Cookbook");
 
-        firebaseDatabase.getReference("app_title").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.e(TAG, "App title updated");
-
-                String appTitle = dataSnapshot.getValue(String.class);
-
-                getSupportActionBar().setTitle(appTitle);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.e(TAG, "Failed to read app title value.", error.toException());
-            }
-        });
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (TextUtils.isEmpty(recipeId)) {
-
-                } else {
-                    //updateCategoryRecipes(recipe, photoUrl);
-                }
-            }
-        });
-
-        toggleButton();
+        btnSave.setOnClickListener(oclBtnSave);
     }
 
-    private void toggleButton() {
-        if (TextUtils.isEmpty(recipeId)) {
-            btnSave.setText("Save");
-        } else {
-            btnSave.setText("Update");
+    public void photoFromCamera() {
+        File file = new File(pictureImagePath);
+        Uri outputFileUri = Uri.fromFile(file);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(cameraIntent, CAMERA_RESULT);
+    }
+
+    private void performCrop(Uri picUri) {
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/4" + ".jpg");
+            Uri outputFileUri = Uri.fromFile(file);
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(picUri, "image/*");
+            cropIntent.putExtra("crop", true);
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 660);
+            cropIntent.putExtra("outputY", 480);
+            cropIntent.putExtra("return-data", true);
+            cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            startActivityForResult(cropIntent, PIC_CROP);
+        } catch (ActivityNotFoundException anfe) {
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
         }
     }
 
-
-    private void addUserChangeListener() {
-        databaseReference.child(recipeId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                CategoryRecipes сategoryRecipes = dataSnapshot.getValue(CategoryRecipes.class);
-
-                if (сategoryRecipes == null) {
-                    Log.e(TAG, "data is null!");
-                    return;
-                }
-
-                Log.e(TAG, "data is changed!" + сategoryRecipes.getName() + ", " + сategoryRecipes.getPhotoUrl());
-                txtDetails.setText(сategoryRecipes.getName() + ", " + сategoryRecipes.getPhotoUrl());
-                inputCategoryName.setText("");
-                toggleButton();
+    View.OnClickListener oclBtnSave = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (downloadUrlCamera != null) {
+                CategoryRecipes categoryRecipes = new CategoryRecipes(inputCategoryName.getText().toString(), downloadUrlCamera.toString());
+                String recipeId = databaseReference.push().getKey();
+                databaseReference.child(recipeId).setValue(categoryRecipes);
+                Toast.makeText(getApplicationContext(), "Дані збережено.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Щось ви робите неправильно, зробіть нормально!", Toast.LENGTH_LONG).show();
             }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.e(TAG, "Failed to read user", error.toException());
-            }
-        });
-    }
-
-    public Bitmap getBitmapfromByteArray(byte[] bitmap) {
-        return BitmapFactory.decodeByteArray(bitmap , 0, bitmap.length);
-    }
+            txtDetails.setText(inputCategoryName.getText().toString());
+            inputCategoryName.setText("");
+        }
+    };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        imageView = (ImageView) findViewById(R.id.photo_imageView);
-        final String recipe = inputCategoryName.getText().toString();
+        ImageView imageView = (ImageView) findViewById(R.id.photo_imageView);
+
         switch (requestCode) {
             case GALLERY_REQUEST:
                 if (resultCode == RESULT_OK) {
                     Uri selectedImage = imageReturnedIntent.getData();
-                    StorageReference photoGalleryRef = storageReference.child(selectedImage.getLastPathSegment());
-
-                    photoGalleryRef.putFile(selectedImage).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            downloadUrl = taskSnapshot.getDownloadUrl();
-                            categoryRecipes = new CategoryRecipes(inputCategoryName.getText().toString(), downloadUrl.toString());
-                            recipeId = databaseReference.push().getKey();
-                            databaseReference.child(recipeId).setValue(categoryRecipes);
-                        }
-                    });
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                        imageView.setImageBitmap(bitmap);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    performCrop(selectedImage);
                 }
             case CAMERA_RESULT:
 
                 if (requestCode == CAMERA_RESULT) {
-                    File imgFile = new  File(pictureImagePath);
-                        Bitmap cameraBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    performCrop(Uri.fromFile(new File(pictureImagePath)));
+                }
+            case PIC_CROP:
+                if (requestCode == PIC_CROP) {
+                    if (imageReturnedIntent != null) {
+                        File imgFile = new  File(Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/4" + ".jpg");
+                        Bitmap selectedBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                        byte[] data = baos.toByteArray();
+                        final Random random = new Random();
 
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    cameraBitmap.compress(Bitmap.CompressFormat.PNG, 0, baos);
-                    byte[] data = baos.toByteArray();
-                    final Random random = new Random();
+                        UploadTask uploadTask = storageReference.child("Photo_Сategory_Recipes" + String.valueOf(random.nextInt())).putBytes(data);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
 
-                    UploadTask uploadTask = storageReference.child("Photo_Сategory_Recipes" + String.valueOf(random.nextInt())).putBytes(data);
-                    uploadTask.addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-
-                        }
-                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Uri downloadUrlCamera = taskSnapshot.getDownloadUrl();
-                            categoryRecipes = new CategoryRecipes(inputCategoryName.getText().toString(), downloadUrlCamera.toString());
-                            recipeId = databaseReference.push().getKey();
-                            databaseReference.child(recipeId).setValue(categoryRecipes);
-                        }
-                    });
-                    imageView.setImageBitmap(cameraBitmap);
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                downloadUrlCamera = taskSnapshot.getDownloadUrl();
+                            }
+                        });
+                        imageView.setImageBitmap(selectedBitmap);
+                    }
                 }
         }
     }
-
 }
