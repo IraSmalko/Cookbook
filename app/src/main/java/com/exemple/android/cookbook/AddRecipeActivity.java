42171cbb6f191dd1ec86434e2b98f79d0f19896f
Ira <smalko.irina.s@gmail.com>
2017-01-28 01:06:20 +0200

    UI for AddRecipeActivity

--- app/src/main/java/com/exemple/android/cookbook/AddRecipeActivity.java
+++ app/src/main/java/com/exemple/android/cookbook/AddRecipeActivity.java
@@ -1,140 +1,203 @@
 package com.exemple.android.cookbook;
 
 
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.exemple.android.cookbook.supporting.CategoryRecipes;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.Random;
 
 public class AddRecipeActivity extends AppCompatActivity{
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private TextView txtDetails;
-    private EditText inputName, inputPhotoUrl, inputIngredients;
-    private Button btnSave;
-    private DatabaseReference mFirebaseDatabase;
-    private FirebaseDatabase mFirebaseInstance;
+    private static final int GALLERY_REQUEST = 1;
+    private static final int CAMERA_RESULT = 0;
+    private static final int PIC_CROP = 2;
 
-    private String recipeId;
+    private EditText inputNameRecipe, inputIngredients;
+    private ImageView imageView;
+    private ProgressDialog progressDialog;
+    private ActionBar actionBar;
+
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    private Uri downloadUrlCamera;
+    private int i = 1;
+    private String pictureImagePath = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/" + i + ".jpg";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_recipe);
 
-        txtDetails = (TextView) findViewById(R.id.txt_category_recipe);
-        inputName = (EditText) findViewById(R.id.name);
-        inputPhotoUrl = (EditText) findViewById(R.id.photoUrl);
-        inputIngredients = (EditText) findViewById(R.id.ingredients);
-        btnSave = (Button) findViewById(R.id.btn_save);
+        imageView = (ImageView) findViewById(R.id.photo_imageView);
+        inputNameRecipe = (EditText) findViewById(R.id.name);
+        inputIngredients = (EditText) findViewById(R.id.add_ingredients);
+        ImageButton btnPhoto_fromGallery = (ImageButton) findViewById(R.id.categoryRecipesPhotoUrlGallery);
+        ImageButton btnPhoto_fromCamera = (ImageButton) findViewById(R.id.categoryRecipesPhotoUrl);
+        Button btnSave = (Button) findViewById(R.id.btn_save);
 
-        mFirebaseInstance = FirebaseDatabase.getInstance();
+        actionBar = getSupportActionBar();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Завантаження");
 
-        mFirebaseDatabase = mFirebaseInstance.getReference("Сategory_Recipes");
-
-        mFirebaseInstance.getReference("app_title").setValue("Cookbook");
-
-        mFirebaseInstance.getReference("app_title").addValueEventListener(new ValueEventListener() {
+        btnPhoto_fromGallery.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.e(TAG, "App title updated");
-
-                String appTitle = dataSnapshot.getValue(String.class);
-
-                getSupportActionBar().setTitle(appTitle);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.e(TAG, "Failed to read app title value.", error.toException());
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
             }
         });
 
-        btnSave.setOnClickListener(new View.OnClickListener() {
+        btnPhoto_fromCamera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String recipe = inputName.getText().toString();
-                String photoUrl = inputPhotoUrl.getText().toString();
-                String description = inputIngredients.getText().toString();
+            public void onClick(View v) {
+                photoFromCamera();
+            }
+        });
 
-                if (TextUtils.isEmpty(recipeId)) {
-                    createUser(recipe, photoUrl);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+
+        databaseReference = firebaseDatabase.getReference("Сategory_Recipes");
+        storageReference = firebaseStorage.getReference().child("Photo_Сategory_Recipes");
+        firebaseDatabase.getReference("app_title").setValue("Cookbook");
+
+        btnSave.setOnClickListener(oclBtnSave);
+    }
+
+
+    public void photoFromCamera() {
+        File file = new File(pictureImagePath);
+        Uri outputFileUri = Uri.fromFile(file);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(cameraIntent, CAMERA_RESULT);
+    }
+
+    private void performCrop(Uri picUri) {
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/4" + ".jpg");
+            Uri outputFileUri = Uri.fromFile(file);
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(picUri, "image/*");
+            cropIntent.putExtra("crop", true);
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 670);
+            cropIntent.putExtra("outputY", 480);
+            cropIntent.putExtra("return-data", true);
+            cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            startActivityForResult(cropIntent, PIC_CROP);
+        } catch (ActivityNotFoundException anfe) {
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    View.OnClickListener oclBtnSave = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (inputNameRecipe.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "Додайте ім'я категорії!", Toast.LENGTH_SHORT).show();
+            }else {
+                if (downloadUrlCamera != null) {
+                    CategoryRecipes categoryRecipes = new CategoryRecipes(inputNameRecipe.getText().toString(), downloadUrlCamera.toString());
+                    String recipeId = databaseReference.push().getKey();
+                    databaseReference.child(recipeId).setValue(categoryRecipes);
+                    Toast.makeText(getApplicationContext(), "Дані збережено.", Toast.LENGTH_SHORT).show();
+                    imageView.setImageResource(R.drawable.dishes);
+                    inputNameRecipe.setText("");
+                    inputIngredients.setText("");
+                    inputIngredients.setVisibility(View.GONE);
+                    inputNameRecipe.setHint("Опис кроку № " + i);
+                    actionBar.setTitle("Крок " + i);
                 } else {
-                    updateCategoryRecipes(recipe, photoUrl);
+                    Toast.makeText(getApplicationContext(), "Щось ви робите неправильно, \n зробіть нормально!", Toast.LENGTH_LONG).show();
                 }
             }
-        });
-
-        toggleButton();
-    }
-
-    // Changing button text
-    private void toggleButton() {
-        if (TextUtils.isEmpty(recipeId)) {
-            btnSave.setText("Save");
-        } else {
-            btnSave.setText("Update");
         }
-    }
+    };
 
-    private void createUser(String name, String photoUrl) {
-        if (TextUtils.isEmpty(recipeId)) {
-            recipeId = mFirebaseDatabase.push().getKey();
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
 
-        CategoryRecipes user = new CategoryRecipes(name, photoUrl);
-
-        mFirebaseDatabase.child(recipeId).setValue(user);
-
-        addUserChangeListener();
-    }
-
-    private void addUserChangeListener() {
-        mFirebaseDatabase.child(recipeId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                CategoryRecipes сategoryRecipes = dataSnapshot.getValue(CategoryRecipes.class);
-
-                if (сategoryRecipes == null) {
-                    Log.e(TAG, "User data is null!");
-                    return;
+        switch (requestCode) {
+            case GALLERY_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    performCrop(selectedImage);
                 }
+            case CAMERA_RESULT:
 
-                Log.e(TAG, "User data is changed!" + сategoryRecipes.getName() + ", " + сategoryRecipes.getPhotoUrl());
+                if (requestCode == CAMERA_RESULT) {
+                    performCrop(Uri.fromFile(new File(pictureImagePath)));
+                }
+            case PIC_CROP:
+                if (requestCode == PIC_CROP) {
+                    if (imageReturnedIntent != null) {
 
-                txtDetails.setText(сategoryRecipes.getName() + ", " + сategoryRecipes.getPhotoUrl());
+                        File imgFile = new File(Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES).getAbsolutePath() + "/4" + ".jpg");
+                        Bitmap selectedBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                        byte[] data = baos.toByteArray();
+                        final Random random = new Random();
+                        progressDialog.show();
 
-                inputPhotoUrl.setText("");
-                inputName.setText("");
-
-                toggleButton();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Log.e(TAG, "Failed to read user", error.toException());
-            }
-        });
-    }
-
-    private void updateCategoryRecipes(String name, String photoUrl) {
-        // updating the user via child nodes
-        if (!TextUtils.isEmpty(name))
-            mFirebaseDatabase.child(recipeId).child("name").setValue(name);
-
-//        if (!TextUtils.isEmpty(photoUrl))
-//            mFirebaseDatabase.child(userId).child("photoUrl").setValue(photoUrl);
+                        UploadTask uploadTask = storageReference.child("Photo_Сategory_Recipes" + String.valueOf(random.nextInt())).putBytes(data);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                progressDialog.dismiss();
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                downloadUrlCamera = taskSnapshot.getDownloadUrl();
+                                progressDialog.dismiss();
+                            }
+                        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                progressDialog.setMessage("Зачекайте, будь ласка" );
+                            }
+                        });
+                        imageView.setImageBitmap(selectedBitmap);
+                    }
+                }
+        }
     }
 }
